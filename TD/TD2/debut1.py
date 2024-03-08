2f472f4fa86a5d5195b468c03f44d7c763661477
Ines Scholler <miuscholler.li@gmail.com>
2024-03-08 10:43:49 +0100

    save 1

--- TD/TD2/debut1.py
+++ TD/TD2/debut1.py
@@ -9,16 +9,13 @@ canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
 
 # Début de votre code
 x = CANVAS_WIDTH / 2
-x0 = 100
-x1 = CANVAS_WIDTH - 100
-y1 = CANVAS_HEIGHT - 100
-y0 = 100
+y1 = CANVAS_HEIGHT - 70
+y0 = 70
 y = CANVAS_HEIGHT / 2
 canvas.create_line(x, y0, x, y1)
-canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
-canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
-canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
-    
+canvas.create_oval(x - 50, y0 + 50, x + 50, y0 - 50)
+canvas.create_oval(x - 50, y1 + 50, x + 50, y1 - 50)
+canvas.create_oval(x + 50, (y0 + y1) / 2 - 50, x - 50, (y0 + y1) / 2 + 50)
 # Fin de votre code
 
 canvas.grid()
